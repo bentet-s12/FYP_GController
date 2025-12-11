053cc8537c534e92fcb4c571cada14e3e696ac24
unknown <aldent10001@gmail.com>
2025-12-11 23:56:33 +0800

    new commit

--- ML_FILES/Actions.py
+++ ML_FILES/Actions.py
@@ -76,6 +76,8 @@ class Actions:
         self.holdvalue = False
         if self._is_holding:
             self._is_holding = False
+        if not self._key_pressed:
+            return
         # keyUp to guarantee release
         if self._key_pressed not in ("left", "right", "middle"):
             pydirectinput.keyUp(self._key_pressed)
