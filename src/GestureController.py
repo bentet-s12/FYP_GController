c536d50f1c99bc9e7e4e316e6d5c573b984a442c
Nedlanox <aldent10001@gmail.com>
2026-02-14 02:27:03 +0800

    final

--- src/GestureController.py
+++ src/GestureController.py
@@ -2,13 +2,8 @@ import importlib
 import os
 import subprocess
 import sys
-from pathlib import Path
-from designerapp.D_UI import run
 import shutil
-
-# =========================
-# REQUIRED DEPENDENCIES
-# =========================
+from designerapp.D_UI import run
 REQUIRED = {
     "PySide6": "PySide6",
     "numpy": "numpy",
@@ -21,22 +16,21 @@ REQUIRED = {
     "PIL": "Pillow",
 }
 
+project_root = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.join(project_root, "src")
+sys.path.insert(0, src_path)
+
 def find_python():
     py = shutil.which("py")
     if py:
         return py
-
     pyexe = shutil.which("python")
     if pyexe:
         return pyexe
-
     py3 = shutil.which("python3")
     if py3:
         return py3
-
-    raise RuntimeError(
-        "Python not found. Install Python 3.10 or 3.11 and ensure it is in PATH."
-    )
+    raise RuntimeError("Python not found. Install Python 3.10/3.11 and add it to PATH.")
 
 def ensure_dependencies(python_cmd):
     missing = []
@@ -51,19 +45,11 @@ def ensure_dependencies(python_cmd):
         return
 
     print("[INFO] Installing missing dependencies:", missing)
-
     subprocess.check_call([python_cmd, "-m", "pip", "install", "--upgrade", *missing])
-
-def run_app():
-    # Add src to sys.path
-    project_root = Path(__file__).resolve().parent
-    src_path = project_root / "src"
-    sys.path.insert(0, str(src_path))
-
-    
-    run()
 
 if __name__ == "__main__":
     py = find_python()
     ensure_dependencies(py)
-    run_app()
+
+    
+    run()
